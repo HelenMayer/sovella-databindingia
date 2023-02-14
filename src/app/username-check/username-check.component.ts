e2edb65cfeaee1a386d77d2ab94e9bbe7450bb34
HelenMayer <you@example.com>
2023-02-14 13:39:29 +0200

    add new project

---
+++ src/app/username-check/username-check.component.ts
@@ -0,0 +1,26 @@
+import { Component, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-username-check',
+  templateUrl: './username-check.component.html',
+  styleUrls: ['./username-check.component.css']
+})
+export class UsernameCheckComponent implements OnInit {
+
+  public userName = "";
+  form: any;
+
+  constructor() {}
+
+  ngOnInit(): void {}  
+
+}
+
+
+
+
+
+
+
+
+
